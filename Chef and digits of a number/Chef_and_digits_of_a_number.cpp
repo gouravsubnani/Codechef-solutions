9183f1b3ee3d25da434d5af4dbb3f2cd9a0a1367
KNuckleeHEADD <97581969+KNuckleeHEADD@users.noreply.github.com>
2022-12-27 16:05:26 +0530

    Update Chef_and_digits_of_a_number.cpp

--- Chef and digits of a number/Chef_and_digits_of_a_number.cpp
+++ Chef and digits of a number/Chef_and_digits_of_a_number.cpp
@@ -1,71 +1,7 @@
-// Gourav Subnani
-#include <iostream>
 #include <bits/stdc++.h>
-using namespace std;
 #define ll long long int
 #define mp map<char,ll>
-#define vec vector<ll>
-#define sett set<ll>
-#define mod 998244353
-
-
-void printmap(mp m)
-{
-    for(auto value : m)
-    {
-        cout<<value.first<<" "<<value.second<<endl;
-    }
-}
-
-void printvec(vec v)
-{
-	for(auto value: v)
-	{
-		cout<<value<<" ";
-	}
-    cout<<endl;
-}
-
-bool poweroftwo(long long x)
-{
-    if(x&(x-1))
-    {
-        return false;
-    }
-    else
-    {
-        return true;
-    } 
-}
-
-void printbinary(int num)
-{
-    for(int i = 31 ; i>=0 ; i--)
-    {
-        cout<<((num>>i)&1);
-    }
-    cout<<endl;
-}
-
-int modmultiply(int a, int b, int c, int d) {
-    int x = 1;
-    x = ((x % mod) * (a % mod)) % mod;
-    x = ((x % mod) * (b % mod)) % mod;
-    x = ((x % mod) * (c % mod)) % mod;
-    x = ((x % mod) * (d % mod)) % mod;
-    return x;
-}
-
-/*****************************BIT MANIPULATION****************************************
-UNSET TO SET = (a | (1<<i)) (ith bit set of a)
-SET TO UNSET = (a & (~(1<<i))) (ith bit unset of a)
-toggle ith bit of a = (a^(1<<i))
-setbit count(int) = __builtin_popcount(a)
-setbit count(ll) = __builtin_popcountll(a)
-lower case to uppercase = (a&'_')
-upper case to lowercase = (A|' ') 
-*************************************************************************************/
-
+using namespace std;
 
 int main() 
 {
@@ -92,4 +28,3 @@ int main()
 	}
 	return 0;
 }
-
